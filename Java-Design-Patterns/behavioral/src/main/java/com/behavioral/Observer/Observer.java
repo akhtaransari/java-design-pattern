8a53e72d677c13affcfc6e6d3dc2ab9b479e5fc5
home <akhtaransari.info@gmail.com>
2026-01-15 15:18:20 +0530

    enhancing my design pattern skillls

---
+++ Java-Design-Patterns/behavioral/src/main/java/com/behavioral/Observer/Observer.java
@@ -0,0 +1,11 @@
+package com.behavioral.Observer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Observer {
+    static void main() {
+        SpringApplication.run(Observer.class);
+    }
+}
---
+++ Java-Design-Patterns/structural/src/main/java/com/structural/AdapterPattern/AdapterPattern.java
@@ -0,0 +1,11 @@
+package com.structural.AdapterPattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdapterPattern {
+    static void main() {
+        SpringApplication.run(AdapterPattern.class);
+    }
+}
