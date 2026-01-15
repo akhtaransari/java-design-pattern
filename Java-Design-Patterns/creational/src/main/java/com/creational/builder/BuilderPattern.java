05253a5e09dddd7a2f96fd0f7d0936c19a0f38cc
home <akhtaransari.info@gmail.com>
2026-01-15 16:13:32 +0530

    added Builder Pattern

--- Java-Design-Patterns/creational/src/main/java/com/creational/builder/BuilderPattern.java
+++ Java-Design-Patterns/creational/src/main/java/com/creational/builder/BuilderPattern.java
@@ -7,6 +7,19 @@ import java.util.stream.Stream;
 
 import static com.creational.builder.Book.*;
 
+
+/*
+A Builder is used to create an object by initializing only the parts that are required,
+like title and publisher.
+
+To implement a builder, we need to create a static class inside the Book class
+(or any class we want to build) with the same fields as the Book class.
+
+Each setter method in the Builder class returns the Builder itself, allowing method chaining.
+At the end, we create a build() method which sets the values of the Book class
+from the Builder class and returns the constructed Book object.
+*/
+
 @SpringBootApplication
 public class BuilderPattern {
     static void main() {
