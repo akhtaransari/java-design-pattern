74d1deaf03a63077e57d00b8ca615af301fb6b52
home <akhtaransari.info@gmail.com>
2026-01-15 15:26:48 +0530

    add modules

--- Java-Design-Patterns/creational/src/main/java/com/creational/builder/Builder.java
+++ Java-Design-Patterns/creational/src/main/java/com/creational/builder/Builder.java
@@ -1,4 +1,4 @@
-package com.structural.AdapterPattern;
+package com.creational.builder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
